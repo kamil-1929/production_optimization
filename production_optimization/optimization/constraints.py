46f409d4cb4fe0471a54e0aaea753a9b3010d9e9
x23233982 <x23233982@student.ncirl.ie>
2024-06-23 21:02:14 +0100

    Update constraints.py

--- production_optimization/optimization/constraints.py
+++ production_optimization/optimization/constraints.py
@@ -2,7 +2,7 @@ import pulp as pl
 
 def add_constraints(model, x1, x2, x3, x4, x5):
     """
-    Adds constraints to the linear programming model.
+    Adds constraints to the linear programming model .
     """
     model += 2 * x1 + 3 * x2 + x3 + 4 * x4 + 5 * x5 <= 100, "Labor_Constraint"
     model += 3 * x1 + 2 * x2 + 4 * x3 + x4 + 3 * x5 <= 80, "Machine_Time_Constraint"
